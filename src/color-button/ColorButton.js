35ba1cffc9e4ea03f6373d208f77af8d0339bdd6
RPRAVE14 <rprave14@ford.com>
2023-09-12 12:31:21 +0530

    Cleanup

--- src/color-button/ColorButton.js
+++ src/color-button/ColorButton.js
@@ -9,18 +9,16 @@ function ColorButton() {
     const [disabled, setDisabled] = useState(false);
 
     return <div id="page">
-        <button
-            style={{backgroundColor: disabled ? 'gray' : buttonColor}}
-            disabled={disabled}
-            onClick={() => setButtonColor(newButtonColor)}
+        <button style={{backgroundColor: disabled ? 'gray' : buttonColor}}
+                disabled={disabled}
+                onClick={() => setButtonColor(newButtonColor)}
         >Change to {newButtonColor}
         </button>
         <br/>
-        <input
-            type='checkbox'
-            data-testid="disableButton-checkbox"
-            id='disableButton-checkbox'
-            onChange={(event) => setDisabled(event.target.checked)}
+        <input type='checkbox'
+               data-testid="disableButton-checkbox"
+               id='disableButton-checkbox'
+               onChange={(event) => setDisabled(event.target.checked)}
         />
         <label htmlFor='disableButton-checkbox'>Disable Button</label>
     </div>;
